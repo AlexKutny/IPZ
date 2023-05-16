b6a203160fedf02eceac5b176fbdb8e05c611b74
Alex <40667270+AlexKutny@users.noreply.github.com>
2023-05-16 08:42:16 +0300

    Add files via upload

---
+++ PropertySearchApp/wwwroot/js/site.js
@@ -0,0 +1,17 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+var password = document.getElementById("inputPassword")
+    , confirm_password = document.getElementById("repeatPassword");
+
+function validatePassword() {
+    if (password.value != confirm_password.value) {
+        confirm_password.setCustomValidity("Passwords Don't Match");
+    } else {
+        confirm_password.setCustomValidity('');
+    }
+}
+
+password.onchange = validatePassword;
+confirm_password.onkeyup = validatePassword;
